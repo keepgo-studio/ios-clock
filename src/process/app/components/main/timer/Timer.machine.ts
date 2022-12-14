1de4abc102770dfca636b0d96f64620c9529de74
keepgo-studio <keepgo.studio@gmail.com>
2022-12-15 01:09:30 +0900

    Add ios-ui-picker

--- src/process/app/components/main/timer/Timer.machine.ts
+++ src/process/app/components/main/timer/Timer.machine.ts
@@ -1,5 +1,34 @@
 import { createMachine } from "xstate";
 
-const TimerMachine = createMachine({});
+const TimerMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBbMAnA+ughgMYAWqAdmAHSoQA2YAxLMvlsgNoAMAuoqAA4B7WKjSCyfEAA9EAJgDsANkqzZAZgCcADnkBGACz7FuzroA0IAJ6JFAVkprHa2xvmytGxZ0UaAvr4s0TFwCEnIqLABXMjJyKCZkQX4uXiQQIRExCTSZBDVZfUp5eU4tRXlPd1ldWwtrBFlOWUp9XUUfE041RS1OYv9AjGw8IlIKSn58SNhGLDhIzBTJDNFUcUlc1WbDBXzbHv2yurkmlraOzi6evvkBkCDh0LGqSenGQnwyQjBaJbSVrIbRAlBxORqcWycfSOfQaY4IXSIoqwjQaWxaRy6TQVO4PEKjcKUKIxOIMD5fH5-ATCVbrHLAzigxzgyHQtRGeGI3TI1FojFqLGuPx3MiCCBwSR4kZhCjLGmA+kNZpOfKXVkw2zyeEAWg0zVkkMuW30EK0ulkuKG+JlVBo9DlmTW2VAm0UzS0WlshjUfQMtl0nh19nkKO0Gn0WnUqItAXuVulzyJ0ViZCgDtpzukcjdlA9Xuhvv0-rhVkQWO5l0rdn9im6alusalT0Jrxm6YVLuz7s93sL-s98IUjIq501JjdxX0-n8QA */
+  createMachine({
+    id: "timer_machine",
+    predictableActionArguments: true,
+    tsTypes: {} as import("./Timer.machine.typegen").Typegen0,
+    states: {
+      idle: {
+        on: {
+          start: "running",
+        },
+      },
+
+      running: {
+        on: {
+          stop: "pause",
+          cancel: "idle",
+        },
+      },
+
+      pause: {
+        on: {
+          resume: "running",
+          cancel: "idle",
+        },
+      },
+    },
+
+    initial: "idle",
+  });
 
 export default TimerMachine;
--- src/process/app/components/main/timer/Timer.ts
+++ src/process/app/components/main/timer/Timer.ts
@@ -1,5 +1,8 @@
 import { html, LitElement, unsafeCSS } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
+import { Picker } from "ios-ui";
+import { interpret } from "xstate";
+import TimerMachine from "./Timer.machine";
 
 import ArrowRightSVG from "@assets/chevron.right.svg";
 import styles from "./Timer.scss";
@@ -7,26 +10,81 @@ import styles from "./Timer.scss";
 import "@widgets/Dial";
 import "@widgets/BottomUpSheet";
 
+Picker.config("picker");
+
+const service = interpret(TimerMachine);
+
 @customElement("app-timer")
 class Timer extends LitElement {
   static styles = unsafeCSS(styles);
+
+  @property()
+  timerNumbers = [0, 0, 0];
 
   @state()
   selectedSounds = [];
 
   @state()
   show = false;
+
+  @state()
+  _state = service.initialState;
+
+  constructor() {
+    super();
+
+    service.onTransition((s) => (this._state = s)).start();
+  }
+
+  getResultListener({ detail }: CustomEvent<Array<number>>) {
+    this.timerNumbers = [...detail];
+
+    console.log(this.timerNumbers);
+  }
+
   render() {
     return html`
       <div class="container">
-        <app-dial></app-dial>
+        <section class="${this._state.matches("idle") ? "" : "running"}">
+          <ios-ui-picker
+            width="100%"
+            height="100%"
+            num-list="24,24,24"
+            title-list="시간,분,초"
+            allow-key-event="true"
+            event-name="getresult"
+            @getresult=${this.getResultListener.bind(this)}
+          ></ios-ui-picker>
+
+          <div class="running-container">running</div>
+        </section>
 
         <div class="button-container">
-          <button class="cancel ios-btn-cir ios-btn-disable">
+          <button
+            @click=${() => service.send("cancel")}
+            class="cancel ios-btn-cir ${this._state.matches("idle")
+              ? "ios-btn-disable"
+              : ""}"
+          >
             <span>취소</span>
           </button>
-          <button class="start ios-btn-cir">
-            <span>시작</span>
+          <button
+            class="start ${this._state.value} ios-btn-cir"
+            @click=${() => {
+              if (this._state.matches("idle")) {
+                service.send("start");
+              } else if (this._state.matches("running")) {
+                service.send("stop");
+              } else if (this._state.matches("pause")) {
+                service.send("resume");
+              }
+            }}
+          >
+            <span>
+              ${this._state.matches("idle") ? "시작" : ""}
+              ${this._state.matches("running") ? "일시정지" : ""}
+              ${this._state.matches("pause") ? "재개" : ""}
+            </span>
           </button>
         </div>
 
@@ -36,8 +94,8 @@ class Timer extends LitElement {
           <p>
             <span>${"전파 탐지기"}</span>
             <widget-svg
-              .width=${12}
-              .height=${12}
+              .width=${13}
+              .height=${13}
               .data=${ArrowRightSVG}
               .fill=${"#98989E"}
             ></widget-svg>
